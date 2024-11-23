f99e332629503e9835725b5f8874cf3f8d2742f5
Marko Lalovic <ml.markolalovic@gmail.com>
2024-11-23 19:46:55 +0000

    additional vs-code extensions

--- apps/vscode/install-extensions.sh
+++ apps/vscode/install-extensions.sh
@@ -2,3 +2,9 @@
 
 # install extensions in `vs-extensions.txt`
 cat vs-extensions.txt | xargs -L1 code --install-extension
+
+# check installed extensions
+code --list-extensions
+
+# uninstall extension
+# code --uninstall-extension <extension-name>
